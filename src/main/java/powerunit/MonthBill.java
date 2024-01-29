858fbfd6f1749cc4799624bda71a8d8065733a08
RedwoodTechno <consultantdineshkumar@gmail.com>
2024-01-29 19:47:07 +0530

    latest eb commit

--- src/main/java/powerunit/MonthBill.java
+++ src/main/java/powerunit/MonthBill.java
@@ -4,7 +4,8 @@ public class MonthBill {
 
 	public void billing() {
 		System.out.println(1000);
-		System.out.println("made payment");
+		System.out.println("made");
+		System.out.println("payment");
 	}
 
 }
