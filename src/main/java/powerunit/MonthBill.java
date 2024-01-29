53fdbdfbd9534b0af1fa9770acd1f9c01da80ae1
RedwoodTechno <consultantdineshkumar@gmail.com>
2024-01-29 20:21:06 +0530

    first change1111s

--- src/main/java/powerunit/MonthBill.java
+++ src/main/java/powerunit/MonthBill.java
@@ -4,7 +4,7 @@ public class MonthBill {
 
 	public void billing() {
 		System.out.println(1000);
-		System.out.println("made111");
+		System.out.println("11111");
 		System.out.println("payment111");		
 	}
 
